7b682f281d3e14a61672149419402f4864c14bb2
tirya <tirya@DESKTOP-JSKVT5E>
2022-06-24 15:36:30 +0300

    Add project files.

---
+++ webJokesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webJokesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
